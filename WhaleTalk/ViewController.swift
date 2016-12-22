42ea870cb24cf7d11544d21395c194fc18cf8f9c
Tom <thitipol.le@gmail.com>
2016-12-22 10:37:01 +0700

    Initial Commit

---
+++ WhaleTalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhaleTalk
+//
+//  Created by TL on 12/22/2559 BE.
+//  Copyright © 2559 TL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
